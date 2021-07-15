6e9859a308146100b7d0b2659ba05e938cbfb6f0
Luis Javier Cano Benítez <ljanobenitez@gmail.com>
2021-07-15 20:42:59 +0200

    limpiado comentarios

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const goles = () => {return Math.floor(Math.random() * 10)};
 //Mezcla Array
 const shuffledArr = array => array.map(a => ({ sort: Math.random(), value: a })).sort((a, b) => a.sort - b.sort).map(a => a.value);
 
-// console.log('Comienza el torneo')
 
-const euroTeams = ['España', 'Francia', 'Portugal', 'Alemania', 'Italia', 'Inglaterra', 'Finlandia', 'Grecia', 'Islandia', 'Noruega', 'Rusia', 'Suiza', 'Suecia', 'Bélgica', 'Dinamarca', 'Croacia', 'Irlanda', 'Lituania', 'Escocia', 'Serbia', 'Luxemburgo', 'Austria', 'Andorra', 'Malta'];
+//const euroTeams = ['España', 'Francia', 'Portugal', 'Alemania', 'Italia', 'Inglaterra', 'Finlandia', 'Grecia', 'Islandia', 'Noruega', 'Rusia', 'Suiza', 'Suecia', 'Bélgica', 'Dinamarca', 'Croacia', 'Irlanda', 'Lituania', 'Escocia', 'Serbia', 'Luxemburgo', 'Austria', 'Andorra', 'Malta'];
 
 const playoffTeams = ['España', 'Francia', 'Portugal', 'Alemania', 'Italia', 'Inglaterra', 'Finlandia', 'Grecia', 'Islandia', 'Noruega', 'Rusia', 'Suiza', 'Suecia', 'Bélgica', 'Dinamarca', 'Croacia'];
+const randomPlayoffTeams = shuffledArr(playoffTeams)
 
-let randomPlayoffTeams = shuffledArr(playoffTeams)
-
-
-//TODO Cada grupo tenemos que volver a randomizarlo para seleccionar los 4 equipos del octavo
 const octavosA = [randomPlayoffTeams.slice(0, 8)];
 const octavosB = [randomPlayoffTeams.slice(8, 16)];
 
@@ -24,21 +20,11 @@ const cuartosB = [];
 const semifinalA = [];
 const semifinalB = [];
 
-// console.log(`Los clasificados para participar son:`)
-// playoffTeams.forEach(team => {
-    // console.log(team)
-// });
-
-
 //TODO Vamos a hacer una clase constructora para estadisticas de equipo estableciendo los valores iniciales a 0
-//TODO Tenemos que randomizar el array de equipos para dividirlo en 2 grupos de 8
-//Crear un array con los clasificados para los cuartos
+//TODO Vamos a hacer una clase constructora para los partidos
 //TODO Por cada grupo de 2 hay que simular una partida con valores random
-//Crear un array con los clasificados para la semifinal
 
 
-
+console.log('Comienza el torneo')
 console.log(`Estas son las selecciones del grupo Octavos A: ${octavosA}`);
 console.log(`Estas son las selecciones del grupo Octavos B: ${octavosB}`);
-// console.log('en el grupo A ' + octavosA)
-// console.log('en el grupo B ' + octavosB)
